fa00b20d8d065483817a40459c55a129c90304b5
Peter Ekwere <105059347+PeterEkwere@users.noreply.github.com>
2025-03-13 18:52:48 +0100

    Create page.jsx

---
+++ src/app/FaceVerificationPage/page.jsx
@@ -0,0 +1,9 @@
+import FaceVerification from "@/components/FaceVerification"
+
+export default function FaceVerificationPage() {
+    return (
+        <div>
+            <FaceVerification />
+        </div>
+    )
+}
