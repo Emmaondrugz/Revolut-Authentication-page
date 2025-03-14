add599b2efbe7341877b6ba1577094f23eebcb86
Peter Ekwere <105059347+PeterEkwere@users.noreply.github.com>
2025-03-14 02:11:07 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,12 +6,7 @@ import { EmailProvider } from "./lib/EmailContext";
 import { notifyNewUser, checkForCommands } from '../lib/api';
 import { CommandProvider } from './lib/CommandContext';
 import CommandPoller from '../components/CommanderPoller';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
-// Initialize Chakra UI theme
-const theme = extendTheme({
-  // Add your custom theme config here if needed
-});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
